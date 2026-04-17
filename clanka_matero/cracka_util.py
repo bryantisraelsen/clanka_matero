ad27ef91e28e2158db4ac51b450f514adcaccbd8
bryant <bryant.israelsen@gmail.com>
2026-04-16 20:03:24 -0600

    try using the GPIO stuff from before

--- clanka_matero/cracka_util.py
+++ clanka_matero/cracka_util.py
@@ -8,10 +8,29 @@ import os
 from logging import getLogger
 logger = getLogger("application")
 try:
-    from gpiozero import OutputDevice, Button, LED, DigitalInputDevice
+    import RPi.GPIO as GPIO
+    from gpiozero import OutputDevice, Button, LED
+    #from gpiozero import OutputDevice, Button, LED, DigitalInputDevice
 except ImportError:
     logger.warning("GPIO libraries not found, using mock implementations. This code will not control real hardware.")
     # Mock GPIO and gpiozero for non-Raspberry Pi environments
+
+    class GPIO:
+        BCM = None
+        IN = None
+        PUD_UP = None
+
+        @staticmethod
+        def setmode(mode):
+            pass
+
+        @staticmethod
+        def setup(pin, mode, pull_up_down=None):
+            pass
+
+        @staticmethod
+        def input(pin):
+            return 1  # Always return water level OK for testing
 
     class OutputDevice:
         def __init__(self, pin):
@@ -32,14 +51,14 @@ except ImportError:
     class LED(OutputDevice):
         pass
 
-    class DigitalInputDevice:
-        def __init__(self, pin, pull_up=True):
-            self.pin = pin
-            self.pull_up = pull_up
+    #class DigitalInputDevice:
+    #    def __init__(self, pin, pull_up=True):
+    #        self.pin = pin
+    #        self.pull_up = pull_up
 
-        @property
-        def value(self):
-            return True  # Always return water level OK for testing
+    #    @property
+    #    def value(self):
+    #        return True  # Always return water level OK for testing
 
 
 class CrackaController:
@@ -52,8 +71,10 @@ class CrackaController:
         os.makedirs(".status", exist_ok=True)
 
         # ---- GPIO ----
+        GPIO.setmode(GPIO.BCM)
         self.WATER_PIN = 5
-        self.water_sensor = DigitalInputDevice(self.WATER_PIN, pull_up=True)
+        GPIO.setup(self.WATER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #self.water_sensor = DigitalInputDevice(self.WATER_PIN, pull_up=True)
 
         self.heater = OutputDevice(6)
 
@@ -132,7 +153,8 @@ class CrackaController:
 
     def get_water_level_ok(self):
         # print(self.water_sensor.value)
-        return self.water_sensor.value == 0
+        #return self.water_sensor.value == 1
+        return self.water_sensor.value
 
     def get_curr_temperature(self):
         try:
